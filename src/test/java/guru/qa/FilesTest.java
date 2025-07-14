df7cbb621d18bcc7874a9a3b40748d16a15d4d86
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-07-14 21:25:42 +0300

    check pdf,csv, exls

--- src/test/java/guru/qa/FilesTest.java
+++ src/test/java/guru/qa/FilesTest.java
@@ -1,15 +1,41 @@
 package guru.qa;
 
+import com.codeborne.pdftest.PDF;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.BooleanNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import net.sf.jxls.reader.XLSReader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Pdf;
+import org.openqa.selenium.json.Json;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 
 public class FilesTest {
     private ClassLoader cl = FilesTest.class.getClassLoader();
+
 
     @Test
     void unZipFileAndReadAll() throws Exception {
@@ -17,12 +43,39 @@ public class FilesTest {
              ZipInputStream zis = new ZipInputStream(is)) {
             ZipEntry entry;
             while ((entry = zis.getNextEntry()) != null) {
-                System.out.println(entry.getName());
+                String name = entry.getName();
+                if (name.equals("1.pdf")) {
+                    try (PDDocument pdf = PDDocument.load(zis)) {
+                        String text = new PDFTextStripper().getText(pdf);
+                        Assertions.assertTrue(text.contains("Блок питания DEEPCOOL PF500 [R-PF500D-HA0B-EU] черный"));
+
+                    }
+                } else if (name.equals("2.xlsx")) {
+                    try (XSSFWorkbook workbook = new XSSFWorkbook(zis)) {
+                        Sheet sheet = workbook.getSheetAt(0);
+                        Assertions.assertEquals("февраль07", sheet.getSheetName());
+
+                    }
+                } else if (name.equals("3.csv")) {
+                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(zis, StandardCharsets.UTF_8))) {
+                        List<String> lines = reader.lines().collect(Collectors.toList());
+                        Assertions.assertEquals(lines.get(0), 0);
+                        Assertions.assertTrue(lines.isEmpty(), String.valueOf(false));
+
+                    }
+
+                }
             }
         }
     }
-}
-
+        @Test
+        void jsonFileParsingTest() throws IOException {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(new File("src/test/resources/DNS.json"));
+            Assertions.assertEquals("SKU-12345", jsonNode.get("items").get(0).get("productId").asText());
+            System.out.println(jsonNode.get("items").get(0).get("productId").asText());
+        }
+    }
 
 
 
---
+++ src/test/java/guru/qa/PersonJson.java
@@ -0,0 +1,16 @@
+package guru.qa;
+
+public class PersonJson {
+    public String name;
+    public String lastName;
+    public Integer age;
+
+    public PersonJson() {
+    }
+
+    public PersonJson(String name, String lastName, Integer age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
